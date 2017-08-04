a5a2f0fc7bee03da8884a5c48e07f0d11dd4e3ee
Duane Bradbury <dibely@hotmail.com>
2017-08-04 09:39:16 +0100

    Added health network synchronisation

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,17 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
-public class Health : MonoBehaviour {
+public class Health : NetworkBehaviour {
     public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
+
     public RectTransform healthBar;
 
     public void TakeDamage(int amount) {
+        if (!isServer) {
+            return;
+        }
+
         currentHealth -= amount;
         if (currentHealth <= 0) {
             currentHealth = 0;
             Debug.Log("Dead!");
         }
+    }
 
-        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    void OnChangeHealth(int health) {
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
     }
 }
