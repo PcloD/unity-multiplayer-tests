1f930e4e41296ae100941382995c2e00bbbf3f44
Duane Bradbury <dibely@hotmail.com>
2017-08-04 09:22:24 +0100

    Added bullet collision and a health component to the polayer along with a UI for health feedback

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour {
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+    public RectTransform healthBar;
+
+    public void TakeDamage(int amount) {
+        currentHealth -= amount;
+        if (currentHealth <= 0) {
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+}
